7ad6fc2b42fba2c2c60ff3716bb7a9f5c3c34722
Pedro Ramos <pedrowarkenramos@gmail.com>
2024-06-24 21:32:23 -0300

    Alterações no debugger de matrizes

--- DebuggerDeMatrizes/src/main.cpp
+++ DebuggerDeMatrizes/src/main.cpp
@@ -11,28 +11,36 @@
 #include "gl_canvas2d.h"
 using namespace std;
 
-int screenWidth = 500, screenHeight = 500;
+int screenWidth = 800, screenHeight = 600;
 
 struct Square
 {
   Vector2 points[4];
 };
 
-float x = 150, y = 100;
 float l = 100;
+float x = 150, y = 100 + l;
 float alfa;
+float x1 = x + l;
+
 Matrix3x3 mainMatrix;
 
 Square square = {
     Vector2(x, y),
     Vector2(x + l, y),
-    Vector2(x + l, y + l),
-    Vector2(x, y + l)};
+    Vector2(x + l, y - l),
+    Vector2(x, y - l)};
 
 void renderAxis()
 {
   CV::line(-screenWidth / 2, 0, screenWidth / 2, 0);
   CV::line(0, -screenHeight / 2, 0, screenHeight / 2);
+}
+
+void drawXYPoint(float x, float y)
+{
+  CV::color(1, 0, 1);
+  CV::circleFill(x, y, 5, 10);
 }
 
 void renderSquare(Square square)
@@ -62,10 +70,11 @@ Square applyMatrixToSquare(Square square, Matrix3x3 M)
 void render()
 {
   CV::translate(screenWidth / 2, screenHeight / 2);
-  CV::line(0, 0, x, y);
+  // CV::line(x1, 0, x, y);
   CV::color(0, 0, 0);
   renderSquare(square);
   renderAxis();
+  drawXYPoint(square.points[0].x, square.points[0].y);
   Square newSquare = applyMatrixToSquare(square, mainMatrix);
   CV::color(1, 0, 0);
   renderSquare(newSquare);
@@ -87,27 +96,25 @@ void mouse(int button, int state, int wheel, int direction, int x, int y)
 
 int main(void)
 {
-  Vector2 P = Vector2(x, y);
+  Vector2 P = Vector2(x - x1, y);
   P.normalize();
-  Vector2 Q = Vector2(1, 0);
-  alfa = acos(P * Q);
-  Matrix3x3 E = Matrix3x3({{{0.6, 0, 0},
-                            {0, 0.6, 0},
+  Vector2 Q = Vector2(0, 1);
+  // alfa = acos(P * Q);
+  alfa = PI / 3;
+  Vector2 P2 = Vector2(x + l, y - l);
+  float scale = 3.0 / 5.0;
+  Matrix3x3 E = Matrix3x3({{{scale, 0, 0},
+                            {0, scale, 0},
                             {0, 0, 1}}});
   Matrix3x3 R = Matrix3x3({{{cos(alfa), -sin(alfa), 0},
                             {sin(alfa), cos(alfa), 0},
                             {0, 0, 1}}});
-  Matrix3x3 T1 = Matrix3x3({{{1, 0, -x},
-                             {0, 1, -y},
+  Matrix3x3 T1 = Matrix3x3({{{1, 0, -P2.x},
+                             {0, 1, -P2.y},
                              {0, 0, 1}}});
   Matrix3x3 T2 = Matrix3x3({{{1, 0, x},
-                             {0, 1, y},
+                             {0, 1, y - l},
                              {0, 0, 1}}});
-
-  square = applyMatrixToSquare(square, T2 * R * T1);
-  R = Matrix3x3({{{cos(-alfa), -sin(-alfa), 0},
-                  {sin(-alfa), cos(-alfa), 0},
-                  {0, 0, 1}}});
   mainMatrix = T2 * R * E * T1;
   CV::init(screenWidth, screenHeight, "Teste");
   CV::run();
--- Testes/src/main.cpp
+++ Testes/src/main.cpp
@@ -7,24 +7,87 @@
 
 #include "gl_canvas2d.h"
 int screenWidth = 500, screenHeight = 500;
-
 float angInit = 0;
 
-void render()
+void engrenage()
+{
+  CV::translate(250, 250);
+  CV::color(1, 0, 0);
+  float raio1 = 100, raio2 = 120, ang = 0;
+  float raio = raio1;
+  float lastX = raio1 * cos(ang + angInit);
+  float lastY = raio1 * sin(ang + angInit);
+  float passo = (2 * PI) / 80;
+  for (ang; ang <= 2 * PI + passo; ang += passo)
+  {
+    float x1 = raio * cos(ang + angInit);
+    float y1 = raio * sin(ang + angInit);
+    ang += passo;
+    float x2 = raio * cos(ang + angInit);
+    float y2 = raio * sin(ang + angInit);
+    CV::line(x1, y1, x2, y2);
+    CV::line(lastX, lastY, x1, y1);
+    lastX = x2;
+    lastY = y2;
+    if (raio == raio1)
+      raio = raio2;
+    else
+      raio = raio1;
+  }
+  angInit -= 0.01;
+}
+
+void espiral()
 {
   CV::translate(screenWidth / 2, screenHeight / 2);
+  CV::color(0, 0, 0);
   float raio = 0;
-  float ang = 0;
-  CV::color(1, 0, 0);
-  for (int i = 0; i < 2000; i++)
+  for (float a = 0; a <= 10 * PI; a += 0.01)
   {
-    float x = raio * cos(angInit + ang);
-    float y = raio * sin(angInit + ang);
+    float x = raio * cos(a + angInit);
+    float y = raio * sin(a + angInit);
     CV::point(x, y);
-    raio += 0.1;
-    ang -= 0.01;
+    raio += 0.01;
   }
   angInit -= 0.001;
+}
+
+void relogio(int h, int m)
+{
+  CV::translate(screenWidth / 2, screenHeight / 2);
+  CV::color(0, 0, 0);
+  float passo = (2 * PI) / 12;
+  float raio1 = 100;
+  float raio2 = 120;
+  CV::circle(0, 0, raio2, 100);
+  for (float a = 0; a < 2 * PI; a += passo)
+  {
+    float x1 = raio1 * cos(a);
+    float y1 = raio1 * sin(a);
+    float x2 = raio2 * cos(a);
+    float y2 = raio2 * sin(a);
+    CV::line(x1, y1, x2, y2);
+  }
+  float x = 50 * cos(-passo * h + PI / 2);
+  float y = 50 * sin(-passo * h + PI / 2);
+  CV::line(0, 0, x, y);
+  float min = (2 * PI) / 60;
+  x = 80 * cos(-min * m + PI / 2);
+  y = 80 * sin(-min * m + PI / 2);
+  CV::line(0, 0, x, y);
+}
+
+int h, m = 0;
+void render()
+{
+  relogio(h, m);
+  h++;
+  if (h > 12)
+    h = 0;
+  m++;
+  if (m > 60)
+    m = 0;
+  Sleep(500);
 }
 
 void keyboard(int key)
